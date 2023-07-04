93658d452c4ffac6bd732bc0df47f5d392301e90
Alvaro <Roekan03@gmail.com>
2023-07-04 11:56:54 +0200

    manejo de errores

--- curso-react/src/pages/login/Login.jsx
+++ curso-react/src/pages/login/Login.jsx
@@ -5,6 +5,9 @@ import Button from "react-bootstrap/Button";
 import {logUser} from "../../services/apiCalls"
 import {useNavigate} from "react-router-dom"
 
+import './Login.css'
+import {errorCheck}  from '../../services/useful'
+
 
 //alvaro@alvaro.com
 //Abcd1234
@@ -22,6 +25,12 @@ export const Login = () => {
     password:""
   })
 
+  const [userError, setUserError] = useState({
+    email:"",
+    password:"",
+    credentials:""
+  })
+
   const [logged, setLogged]=useState(false)
 
   const inputHandler = ({ target }) => {
@@ -32,12 +41,30 @@ export const Login = () => {
     }));
   };
 
+  const errorHandler = ({target},password1)=>{
+    const{name,value}=target;
+    let message=errorCheck(name, value, password1)
+      
+      setUserError((prevState)=>({
+        ...prevState,
+        [name] : message
+      }))
+  }
+
+
   const submitHandler = (e) => {
     e.preventDefault();
     logUser(userCredentials).then((res) => {
       sessionStorage.setItem("token", res.token);
       setLogged(true);
-    });
+    })
+    .catch(error => {console.log(error)
+    let errorMessage = error.response.data.message
+    setUserError((prevState)=>({
+      ...prevState,
+      credentials:errorMessage
+    }))
+    })
   };
 
   useEffect(() => {
@@ -57,6 +84,7 @@ export const Login = () => {
             lg={4}
           >
             <Form>
+              {userError.credentials ? (<div>{userError.credentials}</div>) : (<div></div>)}
               <Form.Group
                 className="mb-3"
                 controlId="formBasicEmail"
@@ -66,8 +94,11 @@ export const Login = () => {
                   type="email"
                   placeholder="Enter email"
                   name="email"
+                  className={userError.email ? ("errorInput") : ("")}
                   onChange={(e) => inputHandler(e)}
+                  onBlur={(e) => errorHandler(e)}
                 />
+                {userError.email ? (<div>{userError.email}</div>) : (<div></div>)}
                 <Form.Text className="text-muted">
                   We'll never share your email with anyone else.
                 </Form.Text>
@@ -82,8 +113,11 @@ export const Login = () => {
                   type="password"
                   placeholder="Password"
                   name="password"
+                  className={userError.password ? ("errorInput") : ("")}
                   onChange={(e) => inputHandler(e)}
+                  onBlur={(e)=>errorHandler(e)}
                 />
+                {userError.password ? (<div>{userError.password}</div>) : (<div></div>)}
               </Form.Group>
               <Form.Group
                 className="mb-3"
---
+++ curso-react/src/services/useful.js
@@ -0,0 +1,29 @@
+export const errorCheck = (name, value, password1) => {
+
+switch (name) {
+    case "email":
+        if(!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(value)){
+        return "Formato de correo incorrecto"    
+        }
+        break;
+
+    case "password":
+        if(value.length<8){
+            return "La contraseña debe tener al menos 8 caracteres";
+        }else if(!value.match(/[a-z]/)){
+            return "La contraseña debe contener al menos una minúscula";
+        }else if(!value.match(/[A-Z]/)){
+            return "La contraseña debe contener al menos una mayúscula";
+        }
+        
+        break;
+
+    case "password2":
+        if (value!==password1 ){
+            return "Las contraseñas no coinciden";
+        }
+        break;
+    default:
+        return "Formato incorrecto";
+    }
+};
